bc387afc93b3dcaa19742c156645fbdd1ab005cb
ong6 <junxiongong2@gmail.com>
2021-12-09 00:46:53 +0800

    day8 - part2

--- python_code/day8.py
+++ python_code/day8.py
@@ -25,4 +25,62 @@ def part1():
     print(count)
 
 
-part1()
+
+def part2():
+    data = sys.stdin.read().strip().split("\n")
+    count = 0
+
+    for i in data:
+        front, back = i.split('|')
+        front = front.split()
+        back = back.split()
+
+        mapping_dic = {}
+
+
+        for num in front:
+            unique_num = len(num)
+            if unique_num == 2:
+                mapping_dic["1"] = num
+            elif unique_num == 3:
+                mapping_dic["7"] = num
+            elif unique_num == 4:
+                mapping_dic["4"] = num
+            elif unique_num == 7:
+                mapping_dic["8"] = num
+
+        for num in front:   
+            unique_num = len(num)
+
+            if unique_num == 5:
+                partial_five = set(mapping_dic["4"]) - set(mapping_dic["7"])
+                if set(mapping_dic["1"]).issubset(set(num)):
+                    mapping_dic["3"] = num
+                elif partial_five.issubset(set(num)):
+                    mapping_dic["5"] = num
+                else:
+                    mapping_dic["2"] = num
+
+            elif unique_num == 6:
+                partial_zero = (set(mapping_dic["8"]) - set(mapping_dic["4"])) | set(mapping_dic["1"])
+                if set(mapping_dic["4"]).issubset(set(num)):
+                    mapping_dic["9"] = num
+                elif partial_zero.issubset(set(num)):
+                    mapping_dic["0"] = num
+                else:
+                    mapping_dic["6"] = num
+            
+        print(mapping_dic)
+
+        for key, value in mapping_dic.items():
+            for a, b in list(enumerate(back)):
+                if set(value) ==  set(b):
+                    back[a] = key
+
+        output = "".join([str(i) for i in back])
+        count += int(output)
+        print(count)
+
+
+
+part2()
--- python_code/test.py
+++ python_code/test.py
@@ -1,43 +1,15 @@
 # %%
 
-def lengthOfLongestSubstring(s):
-    repeat_check = []
-    max_list = []
-    curr_max = 0
-    for key in s:
-        print(max_list)
-        if key not in repeat_check:
-            repeat_check.append(key)
-            curr_max += 1
-        else:
-            repeat_check = []
-            repeat_check.append(key)
-            max_list.append(curr_max)
-            curr_max = 1
-        
-    max_list.append(curr_max)
-    print(max(max_list))
-        
-input = "abcabcbb" #3
-input2 = "bbbbb" #1
-input3 = "pwwkew" #3
-input4 = ""
-input5 = " "
-input6 = "dvdf"
-
-
-# lengthOfLongestSubstring(input)
-# lengthOfLongestSubstring(input2)
-# lengthOfLongestSubstring(input3)
-# lengthOfLongestSubstring(input4)
-# lengthOfLongestSubstring(input5)
-lengthOfLongestSubstring(input6)
-
+temp = set("abc") - set("cd")
+print(temp)
+temp.issubset(set(['a', 'b', 'c' ,'d' ]) )
 
 # %%
-print(5%0)
-# %%
-[i for i in range(5)]
-# %%
-[i for _ in range(9,7)]
+
+a = ["1" , "2"]
+
+string = "".join([str(i) for i in a])
+print(string)
+
+
 # %%
